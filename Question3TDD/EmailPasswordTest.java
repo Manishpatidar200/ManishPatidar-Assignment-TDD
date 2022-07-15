3c1ad0d82a0afe4be2887bdb73914cd7ae395962
Manishpatidar200 <56249927+Manishpatidar200@users.noreply.github.com>
2022-07-15 11:23:35 +0530

    Update EmailPasswordTest.java

--- Question3TDD/EmailPasswordTest.java
+++ Question3TDD/EmailPasswordTest.java
@@ -11,8 +11,8 @@ class EmailPasswordTest {
 	@Test
 	void test() {
 		EmailPassword test=new EmailPassword();
-		String userName = "balram";
-		String password = "sonu234";
+		String userName = "Manish";
+		String password = "Manish@123";
 		String output=test.isValidUserName(userName,password);
 		assertEquals("true",output);
  
