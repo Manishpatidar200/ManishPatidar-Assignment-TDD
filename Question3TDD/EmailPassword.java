8daa29eaed26ee3dd1882107d948d33e3ab31a13
Manishpatidar200 <56249927+Manishpatidar200@users.noreply.github.com>
2022-07-15 11:24:25 +0530

    Update EmailPassword.java

--- Question3TDD/EmailPassword.java
+++ Question3TDD/EmailPassword.java
@@ -4,8 +4,8 @@ public class EmailPassword {
 
 	 public String isValidUserName(String name,String password){
  
-	        String userName1 = "balram";
-	        String pwd = "sonu234";
+	        String userName1 = "Manish";
+	        String pwd = "Manish@123";
          if(name.equals(userName1) || password.equals(pwd)){
 	            return "true";
 	        }
